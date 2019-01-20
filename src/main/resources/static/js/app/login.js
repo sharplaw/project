d99554a3368fcb3ef71d4e09357fd48265e58657
loong <web_loong@163.com>
2019-01-20 23:35:09 +0800

    2019年1月20日11:26:33   开始制作ipad 功能

--- src/main/resources/static/js/app/login.js
+++ src/main/resources/static/js/app/login.js
@@ -34,7 +34,14 @@ $(document).ready(function () {
 
 });
 
-
+function goPAGE() {
+    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+       return 'pad/login';
+    }
+    else {
+        return 'index';
+    }
+}
 function reloadCode() {
     $("#validateCodeImg").attr("src", ctx + "gifCode?data=" + new Date() + "");
 }
@@ -71,7 +78,8 @@ function login() {
         dataType: "json",
         success: function (r) {
             if (r.code === 0) {
-                location.href = ctx + 'index';
+                var typpe=goPAGE();
+                location.href = ctx + typpe;
             } else {
                 reloadCode();
                 $MB.n_warning(r.msg);
