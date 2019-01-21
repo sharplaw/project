e7b40d33a620e5c8ba97c93ef7a62b9dab7f86c1
sharplaw <316498382@qq.com>
2019-01-21 21:52:58 +0800

    功能

---
+++ src/main/java/cc/mrbird/common/base64/Base.java
@@ -0,0 +1,50 @@
+package cc.mrbird.common.base64;
+
+
+import java.io.*;
+import java.util.Base64;
+ //本地图片转换成base64字符串
+public class Base {
+    public static String imageToBase64Str(String imgFile) {
+        InputStream in = null;
+        byte[] data = null;
+
+        // 读取图片字节数组
+        try {
+            in = new FileInputStream(imgFile);
+
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Base64.Encoder encoder = Base64.getEncoder();
+
+        return encoder.encodeToString(data);
+    }
+ // base64字符串转换成图片
+    public static boolean CreateImage(String imgStr,String path) {
+        if (imgStr == null) // 图像数据为空
+            return false;
+        Base64.Decoder decoder = Base64.getDecoder();
+        try {
+            // Base64解码
+            byte[] b = decoder.decode(imgStr);
+            for (int i = 0; i < b.length; ++i) {
+                if (b[i] < 0) {// 调整异常数据
+                    b[i] += 256;
+                }
+            }
+            // 生成jpeg图片
+            String imgFilePath = path;// 新生成的图片
+            OutputStream out = new FileOutputStream(imgFilePath);
+            out.write(b);
+            out.flush();
+            out.close();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/cc/mrbird/common/base64/PictureController.java
@@ -0,0 +1,41 @@
+package cc.mrbird.common.base64;
+
+import cc.mrbird.common.annotation.Log;
+import cc.mrbird.common.controller.BaseController;
+import cc.mrbird.common.domain.ResponseBo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+public class PictureController  extends BaseController {
+
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+
+    @Log("上传图片")
+    @RequestMapping("picture/upload")
+    @ResponseBody
+    public ResponseBo upload(String imgStr, String prisonerNo, String seet) {
+        Date date = new Date();
+        int month = date.getMonth();
+        int months = month + 1;
+        String mon = String.valueOf(months);
+        String path = "D://" + prisonerNo + "//" + mon + "//" + prisonerNo + "-" + mon + "-" + seet + ".jpg";
+       boolean flag= Base.CreateImage(imgStr, path);
+       if(flag=true){
+           return ResponseBo.ok();
+       }else{
+           return ResponseBo.error();
+       }
+
+    }
+
+
+}
