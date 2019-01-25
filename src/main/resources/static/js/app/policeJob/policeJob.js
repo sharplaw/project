5c20cfcc7c5a552c257cfe30c277531092832bf8
loong <web_loong@163.com>
2019-01-25 13:47:51 +0800

    html生成canvas

--- src/main/resources/static/js/app/policeJob/policeJob.js
+++ src/main/resources/static/js/app/policeJob/policeJob.js
@@ -1,4 +1,4 @@
-var PrionerNo=''
+var PrionerNo='',ID=''
 function updateJob() {
     var selected = $("#jobTable").bootstrapTable("getSelections");
     console.log(selected)
@@ -13,6 +13,7 @@ function updateJob() {
     }
     var sels = selected[0];
     PrionerNo=sels.prisonerNo;
+    ID=sels.id;
     if(sels.statue=='0'){
         $.ajax({
             type:"get",
@@ -102,109 +103,28 @@ $(function () {
 });
 var validator;
 var $jobAddForm = $("#job-add-form");
-(function($){
-    $.fn.serializeObject=function(){
-        var o={};
-        var a=this.serializeArray();
-        $.each(a, function() {
-            if(o[this.name]){
-                if(!o[this.name].push){
-                    o[this.name]=[o[this.name]];
-                }
-                o[this.name].push(this.value||'');
-            }else{
-                o[this.name]=this.value||'';
-            }
-        });
-        return o;
-    };
-})(jQuery);
+
 $(function () {
    // validateRule();
-    $("input[name='date1']").daterangepicker(
-        {
-            singleDatePicker: true,//设置为单个的datepicker，而不是有区间的datepicker 默认false
-            showDropdowns: true,//当设置值为true的时候，允许年份和月份通过下拉框的形式选择 默认false
-            autoUpdateInput: false,//1.当设置为false的时候,不给与默认值(当前时间)2.选择时间时,失去鼠标焦点,不会给与默认值 默认true
-            timePicker24Hour : true,//设置小时为24小时制 默认false
-            timePicker : true,//可选中时分 默认false
-            locale: {
-                format: "YYYY-MM-DD HH:mm:ss",
-                applyLabel: '确定',
-                cancelLabel: '取消',
-                separator: " - ",
-                daysOfWeek: ["日","一","二","三","四","五","六"],
-                monthNames: ["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"]
-            }
-
-        }
-    ).on('cancel.daterangepicker', function(ev, picker) {
-        $("#date1").val("请选择日期");
-        $("#servingStartTime").val("");
-    }).on('apply.daterangepicker', function(ev, picker) {
-        $("#servingStartTime").val(picker.startDate.format('YYYY-MM-DD HH:mm:ss'));
-        $("#date1").val(picker.startDate.format('YYYY-MM-DD HH:mm:ss'));
-    });
-    $("input[name='date2']").daterangepicker(
-        {
-            singleDatePicker: true,//设置为单个的datepicker，而不是有区间的datepicker 默认false
-            showDropdowns: true,//当设置值为true的时候，允许年份和月份通过下拉框的形式选择 默认false
-            autoUpdateInput: false,//1.当设置为false的时候,不给与默认值(当前时间)2.选择时间时,失去鼠标焦点,不会给与默认值 默认true
-            timePicker24Hour : true,//设置小时为24小时制 默认false
-            timePicker : true,//可选中时分 默认false
-            locale: {
-                format: "YYYY-MM-DD HH:mm:ss",
-                applyLabel: '确定',
-                cancelLabel: '取消',
-                separator: " - ",
-                daysOfWeek: ["日","一","二","三","四","五","六"],
-                monthNames: ["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"]
-            }
-
-        }
-    ).on('cancel.daterangepicker', function(ev, picker) {
-        $("#date2").val("请选择日期");
-        $("#servingEndTime").val("");
-    }).on('apply.daterangepicker', function(ev, picker) {
-        $("#servingEndTime").val(picker.startDate.format('YYYY-MM-DD HH:mm:ss'));
-        $("#date2").val(picker.startDate.format('YYYY-MM-DD HH:mm:ss'));
-    });
 
 
     $("#job-add .btn-save").click(function () {
-        var name = $(this).attr("name");
-        validator = $jobAddForm.validate();
-        var flag = validator.form();
-        var jsSerialize=$jobAddForm.serializeObject();
-        console.log(jsSerialize)
-        if (flag) {
+        $.ajax({
+            type:"post",
+            url:ctx + "task/finish",
+            dataType:"json", //预期服务器返回数据的类型
+            data:{id:ID},
+            success:function(r){
+                closeModal();
+                refresh();
+                $MB.n_success(r.msg);
+            },
+            error:function(jqXHR){
+                alert("发生错误："+ jqXHR.status);
 
-            if (name === "save") {
-                // $.post(ctx + "job/update", $jobAddForm.serialize(), function (r) {
-                //     if (r.code === 0) {
-                //         closeModal();
-                //         refresh();
-                //         $MB.n_success(r.msg);
-                //     } else $MB.n_danger(r.msg);
-                // });
-                $.ajax({
-                    type:"post",
-                    url:ctx + "task/update",
-                    dataType:"json", //预期服务器返回数据的类型
-                    data:jsSerialize,
-                    success:function(r){
-                        // window.location.reload()
-                        $("input[name=id]").remove();
-                        closeModal();
-                        refresh();
-                        $MB.n_success(r.msg);
-                    },
-                    error:function(jqXHR){
-                        alert("发生错误："+ jqXHR.status);
-                    }
-                });
             }
-        }
+        });
+
     });
 
     $("#job-add .btn-close").click(function () {
@@ -217,11 +137,9 @@ $(function () {
 });
 
 function closeModal() {
-   // $("#job-add-button").attr("name", "save");
     $MB.closeAndRestModal("job-add");
     $MB.closeAndRestModal("job-add1");
     validator.resetForm();
-    //$("#job-add-modal-title").html('新增任务');
 }
 var timing='';
 function zwsb() {
@@ -327,6 +245,23 @@ function closeShebei() {
             window.setTimeout(function(){
                 $('.spmsg').html('录入完成')
             },500);
+            $.ajax({
+                type:"post",
+                url:ctx + "task/update",
+                dataType:"json", //预期服务器返回数据的类型
+                data:{id:ID,fingerUrl: $("input[name=fingerUrl]").val(r.msg.path)},
+                success:function(r){
+                    window.setTimeout(function(){
+                        $('.spmsg').html('已更新数据')
+                    },500);
+
+                },
+                error:function(jqXHR){
+                    alert("发生错误："+ jqXHR.status);
+                    $('#zhiwMsg').hide();
+                    $('#zhiwerror').show();
+                }
+            });
         },
         error:function(jqXHR){
             alert("发生错误："+ jqXHR.status);
