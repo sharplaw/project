485024566fa1e71fcd0e796272ca8f60570ad0c7
sharplaw <316498382@qq.com>
2019-01-28 00:52:37 +0800

    功能

---
+++ src/main/java/cc/mrbird/common/util/vedio/Contants.java
@@ -0,0 +1,24 @@
+package cc.mrbird.common.util.vedio;
+
+public class Contants {
+    /**
+     * @Description:(3.工具类主类)设置转码工具的各个路径
+     * @param:@param args
+     * @return:void
+     * @author:Zoutao
+     * @date:2018-6-22
+     * @version:V1.0
+     */
+
+    public static final String ffmpegpath = "E:/ffmpeg/bin/ffmpeg.exe";     // ffmpeg工具安装位置
+    public static final String mencoderpath = "E:/ffmpeg/bin/mencoder";     // mencoder工具安装的位置
+
+    public static final String videofolder = "E://Projectpicture/websiteimages/temp/";  // 需要被转换格式的视频目录
+    public static final String videoRealPath = "E://Projectpicture/websiteimages/temp/";    // 需要被截图的视频目录
+
+    public static final String targetfolder = "E://Projectpicture/websiteimages/finshvideo/"; // 转码后视频保存的目录
+    public static final String imageRealPath = "E://Projectpicture/websiteimages/finshimg/"; // 截图的存放目录
+
+
+
+}
