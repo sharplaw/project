2edffa8b6a46e4606f68202d36a7327fe8341fbd
sharplaw <316498382@qq.com>
2019-01-29 00:08:48 +0800

    功能

--- src/main/java/cc/mrbird/Application.java
+++ src/main/java/cc/mrbird/Application.java
@@ -35,7 +35,7 @@ public class Application {
     public MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
         //单个文件最大
-        factory.setMaxFileSize("10240KB"); //KB,MB
+        factory.setMaxFileSize("20480KB"); //KB,MB
         /// 设置总上传数据总大小
         factory.setMaxRequestSize("102400KB");
         return factory.createMultipartConfig();
